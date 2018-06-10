7189a0a136d8fefa6f4ea6b0912d4114c8b32920
shukubota <kubotashu0421@gmail.com>
2018-06-11 04:27:24 +0900

    フロント側整備

---
+++ front_end/containers/Signin.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import ReactDom from 'react-dom'
+
+import Style from './App.css'
+
+export default class Auth extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {
+        email: '',
+        password: '',
+      },
+    };
+  }
+
+  handleChange(prop, e) {
+    console.log(this.state);
+    const { user } = this.state;
+    user[prop] = e.target.value;
+    this.setState({
+      user,
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.state.user);
+    console.log(this.props);
+    this.props.setUser(this.state.user);
+  }
+  render() {
+    const { user } = this.state;
+    console.log(this.props);
+    return (
+      <div>
+        <h1>サインイン</h1>
+        <div>
+          <form onSubmit={this.handleSubmit.bind(this)}>
+            <input type="text" placeholder="メールアドレス" value={user.email} onChange={this.handleChange.bind(this, 'email')}/>
+            <input type="password" placeholder="パスワード" value={user.password} onChange={this.handleChange.bind(this, 'password')}/>
+            <input type="submit" name="commit" value="ログイン" data-disable-with="ログイン"/>
+          </form>
+          <p>
+            test@kmail.com: aaaaa
+          </p>
+        </div>
+       </div>
+    )
+  }
+}
--- front_end/createStore.js
+++ front_end/createStore.js
@@ -1,10 +1,25 @@
 import {
   createStore,
+  applyMiddleware,
   combineReducers,
 } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { routerReducer, routerMiddleware } from 'react-router-redux';
+import createHistory from 'history/createBrowserHistory';
 
 import reducers from './reducers/index';
 
-const store = createStore(reducers);
+
+export const history = createHistory();
+
+const store = createStore(
+  reducers,
+  applyMiddleware(
+    routerMiddleware(history),
+    logger,
+    thunk,
+  ),
+);
 
 export default store;

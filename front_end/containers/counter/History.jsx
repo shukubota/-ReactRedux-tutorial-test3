3adda04dffeb03f76c2d960c875cb9745311d426
shukubota <kubotashu0421@gmail.com>
2018-06-11 07:11:36 +0900

    修正

--- front_end/containers/counter/History.jsx
+++ front_end/containers/counter/History.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Header from './Header';
 
 const History = (props) => {
   const { countHistory } = props;
   const historyMap = countHistory.map(history => <div>{ history }</div>);
   return (
     <div>
+      <Header {...props}/>
       <h2>検索結果一覧</h2>
       <Link to='/'>
         topへ

242aca0ec2e58ffc94a12958f9749ddfc21782c0
shukubota <kubotashu0421@gmail.com>
2018-06-11 04:40:04 +0900

    reducerかく

--- front_end/reducers/index.js
+++ front_end/reducers/index.js
@@ -21,9 +21,29 @@ function fetchUserReducers(state = {}, action) {
   }
 }
 
+function getTime() {
+  const Time = new Date();
+  return `${Time.getHours()}時${Time.getMinutes()}分`;
+}
+
+function handleCount(state = initialState, action) {
+  switch (action.type) {
+    case 'ADD': {
+      const thisHistory = ['ADD', getTime()];
+      return {
+        ...state,
+        count: action.payload.count + 1,
+        history: [action.payload.history, thisHistory],
+      };
+    }
+    default:
+      return state;
+  }
+}
 
 const rootReducers = combineReducers({
   fetchUserReducers,
+  handleCount,
 });
 
 export default rootReducers;
